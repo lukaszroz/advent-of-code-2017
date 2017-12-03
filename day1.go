d2e379b27aae80b67d96f721d6b85acf62989214
Lukasz Rozycki <git@lukaszr.com>
2017-12-02 23:29:23 -0700

    Day 1 2nd star

--- day1.go
+++ day1.go
@@ -7,11 +7,17 @@ import (
 
 func main() {
 	list := os.Args[1]
+
+	fmt.Println("1st star: ", nsum(1, list))
+	fmt.Println("2nd star: ", nsum(len(list) / 2, list))
+}
+
+func nsum(n int, list string) int {
 	sum := 0
 	for i := range list {
-		if(list[i] == list[(i + 1) % len(list)]) {
+		if(list[i] == list[(i + n) % len(list)]) {
 			sum += int(list[i] - '0')
 		}
 	}
-	fmt.Println(sum)
+	return sum
 }
