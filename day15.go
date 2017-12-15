ec5c1647f8743ce709ef942c41af0ce99bad4654
Lukasz Rozycki <git@lukaszr.com>
2017-12-15 10:39:16 -0700

    Day 15 cleanup

--- day15.go
+++ day15.go
@@ -10,11 +10,12 @@ func main() {
 	startA := parseUint(os.Args[1])
 	startB := parseUint(os.Args[2])
 
+	//Closures are faster for both stars on my system
 	fmt.Println("Star 1: ", count(startA, startB, 1, 1, 40000000, false))
 	fmt.Println("Star 2: ", count(startA, startB, 4, 8, 5000000, false))
 }
 
-func count(startA, startB, filterA, filterB uint64, times int, channel bool) int {
+func count(startA, startB, filterA, filterB int, times int, channel bool) int {
 	var factory newGenerator
 	if channel {
 		factory = newGeneratorChannel
@@ -25,14 +26,14 @@ func count(startA, startB, filterA, filterB uint64, times int, channel bool) int
 	genB := factory(startB, 48271, filterB)
 	sum := 0
 	for i := 0; i < times; i++ {
-		if (genA() % 65536) == (genB() % 65536) {
+		if genA() == genB() {
 			sum++
 		}
 	}
 	return sum
 }
 
-func next(n, factor, filter uint64) uint64 {
+func next(n, factor, filter int) int {
 	n *= factor
 	n %= 2147483647
 	for n%filter != 0 {
@@ -42,33 +43,33 @@ func next(n, factor, filter uint64) uint64 {
 	return n
 }
 
-type generator func() uint64
-type newGenerator func(uint64, uint64, uint64) generator
+type generator func() int
+type newGenerator func(int, int, int) generator
 
-func newGeneratorClosure(n, factor, filter uint64) generator {
-	return func() uint64 {
+func newGeneratorClosure(n, factor, filter int) generator {
+	return func() int {
 		n = next(n, factor, filter)
-		return n
+		return n % 65536
 	}
 }
 
-func newGeneratorChannel(start, factor, filter uint64) generator {
-	c := make(chan uint64, 64)
+func newGeneratorChannel(start, factor, filter int) generator {
+	c := make(chan int, 64)
 	go generatorRoutine(start, factor, filter, c)
-	return func() uint64 {
+	return func() int {
 		return <-c
 	}
 }
 
-func generatorRoutine(n, factor, filter uint64, c chan uint64) {
+func generatorRoutine(n, factor, filter int, c chan int) {
 	for {
 		n = next(n, factor, filter)
-		c <- n
+		c <- n % 65536
 	}
 }
 
-func parseUint(s string) (u uint64) {
-	u, err := strconv.ParseUint(s, 10, 64)
+func parseUint(s string) (u int) {
+	u, err := strconv.Atoi(s)
 	if err != nil {
 		panic(err)
 	}
